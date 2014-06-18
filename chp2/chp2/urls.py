5fa25ecb29c12e53387a88ab21391ce33329f77c
Zeph Grunschlag <zgrunschlag@gmail.com>
2014-06-18 14:27:33 -0400

    fixed bug in path

--- chp2/chp2/urls.py
+++ chp2/chp2/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from chpw.views import hello
+from chp2.views import hello
 
 urlpatterns = patterns('',
     # Examples:

32c50b7ab5a47dd8539a92a4c4546baded733932
Zeph Grunschlag <zgrunschlag@gmail.com>
2014-06-18 15:30:56 -0400

    Adding the capability to run development server on port 9999 and testing that capability.

--- chp2/chp2/urls.py
+++ chp2/chp2/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from chp2.views import hello
+from chp2.views import hello, hello_zeph
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'chp2.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^hello/$', hello, name='hello'),
+    url(r'^hello-zeph/$', hello_zeph, name='hello-zeph'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- chp2/chp2/views.py
+++ chp2/chp2/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse("Hello world")
+
+def hello_zeph(request):
+    return HttpResponse("Hello there Zeph!!!!")

101fa44bc3b310fb696d95e67b4903d3bbabbba1
Zeph Grunschlag <zgrunschlag@gmail.com>
2014-06-18 14:55:14 -0400

    Another try

--- chp2/chp2/urls.py
+++ chp2/chp2/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'chp2.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^hello/$', include(hello)),
+    url(r'^hello/$', hello, name='hello'),
     url(r'^admin/', include(admin.site.urls)),
 )
